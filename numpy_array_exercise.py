39b34a573a5d91680a3492947fc3a99ca95dc013
Xianghua Li <37569717+sunflowerlily@users.noreply.github.com>
2025-03-14 23:24:25 +0800

    Update numpy_array_exercise.py

--- numpy_array_exercise.py
+++ numpy_array_exercise.py
@@ -3,5 +3,3 @@ import numpy as np
 def find_power_with_six():
     return np.array([i for i in range(1, 100) if str(2**i)[-1] == '6'])
 
-def first_ten_powers():
-    return np.array([2**i for i in range(10)])
